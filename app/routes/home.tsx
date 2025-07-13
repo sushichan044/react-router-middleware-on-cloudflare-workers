5a981dc7ecc40de5cebc6553dabed6fe653d029b
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-07-13 19:26:19 +0900

    use react router context in route

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import { cloudflareRRCtx } from "../entry.server";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,7 +10,9 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export function loader({ context }: Route.LoaderArgs) {
-  return { message: context.cloudflare.env.VALUE_FROM_CLOUDFLARE };
+  const cf = context.get(cloudflareRRCtx);
+
+  return { message: cf.VALUE_FROM_CLOUDFLARE };
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
