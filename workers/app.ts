19c982ad25bb1ae31b79993ab12772a62edaabed
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2025-07-13 19:16:24 +0900

    scaffold hono server

--- workers/app.ts
+++ workers/app.ts
@@ -1,23 +1,21 @@
 import { createRequestHandler } from "react-router";
 
-declare module "react-router" {
-  export interface AppLoadContext {
-    cloudflare: {
-      env: Env;
-      ctx: ExecutionContext;
-    };
-  }
-}
+import { Hono } from "hono";
 
-const requestHandler = createRequestHandler(
-  () => import("virtual:react-router/server-build"),
-  import.meta.env.MODE
-);
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore virtual module provided by React Router at build time
+import * as build from "virtual:react-router/server-build";
 
-export default {
-  async fetch(request, env, ctx) {
-    return requestHandler(request, {
-      cloudflare: { env, ctx },
-    });
-  },
-} satisfies ExportedHandler<Env>;
+type HonoConfig = {
+  Bindings: CloudflareBindings;
+};
+
+const app = new Hono<HonoConfig>();
+
+const reactRouterHandler = createRequestHandler(build, import.meta.env.MODE);
+
+app.all("*", async (c) => {
+  return reactRouterHandler(c.req.raw);
+});
+
+export default app;
